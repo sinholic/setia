a5b094002f6b7a7ab0fad38539a3f392a787afb6
Mahar Prasetio <mahar@misua.id>
2018-07-05 13:26:49 +0700

    First commit

---
+++ resources/views/admin/main.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="shortcut icon" href="assets/img/favicon.png">
+        <link type="text/css" rel="stylesheet" href="{{ mix('/css/app.css') }}">
+        <title>SETIA</title>
+    </head>
+    <body>
+        <main class="main">
+            
+        </main>
+        @yield('content')
+        <script type="text/javascript" src="{{ mix('/js/app.js') }}"></script>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,88 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::group(['middleware' => ['auth']],function () {
+    // Route::get('/', ['uses' => 'HomeController@index', 'as' => 'home.index']);
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    Route::group([
+            'prefix'=>'admin'
+        ],function () {
+            Route::get('/', ['uses' => 'AdminController@index', 'as' => 'admin.index']);
+            Route::get('improvement', ['uses' => 'HomeController@improvement', 'as' => 'home.improvement']);
+    });
+    Route::group([
+            'prefix'=>'reporting'
+        ],function () {
+        Route::get('/', ['uses' => 'ReportingController@index', 'as' => 'reporting.index']);
+        Route::get('/show/{type?}', ['uses' =>'ReportingController@show', 'as' => 'reporting.show.detail']);
+        Route::get('/download/{type?}', ['uses' =>'ReportingController@download', 'as' => 'reporting.download.detail']);
+		Route::get('monthlyreport',array('as'=>'monthlyreport','uses'=>'ReportingController@monthlyreport'));
+
+        Route::post('/showperdate', ['uses' =>'ReportingController@showperdate', 'as' => 'reporting.activities.show.showperdate']);
+
+    });
+    Route::resource('transitionprogress', 'TransitionprogressController');
+    Route::get('dailycheck', 'AoactivitydailyController@checklistact');
+    Route::get('insertact', 'AoactivitydailyController@insertact');
+    Route::get('issuecheck', 'AoissueController@issuecheck2');
+    Route::resource('aoactivitydaily', 'AoactivitydailyController');
+    Route::get('tickets/{status?}', ['uses' =>'TicketController@index', 'as' => 'tickets.index']);
+    Route::group([
+            'prefix'=>'time-sheet'
+        ],function () {
+        Route::get('activitydetails/type/{activitytypes?}', ['uses' =>'ActivitydetailController@index', 'as' => 'activitydetails.index']);
+        Route::match(['PUT', 'PATCH'], "activitydetails/update/{id?}", [ 'uses' => 'ActivitydetailController@update', 'as' => 'activitydetails.update']);
+        Route::post('activitydetails/add', ['uses' =>'ActivitydetailController@store', 'as' => 'activitydetails.store']);
+        Route::get('activitydetails/show/{id?}', ['uses' =>'ActivitydetailController@show', 'as' => 'activitydetails.show']);
+    });
+    Route::group([
+            'prefix'=>'issue'
+        ],function () {
+        Route::resource('ctmissues', 'CtmissueController');
+		Route::resource('aoissues', 'AoissueController');
+		Route::get('bptoms/viewupdatebptoms', ['uses' =>'BptomsController@viewupdatebptoms', 'as' => 'bptoms.viewupdatebptoms']);
+        Route::resource('bptoms', 'BptomsController');
+        Route::post('bptoms/add', ['uses' =>'BptomsController@view', 'as' => 'bptoms.view']);
+        Route::resource('bptomsbodependent', 'BptomsbodependentController');
+        Route::resource('bptomsboprerequisite', 'BptomsboprerequisiteController');
+    });
+    Route::group([
+            'prefix'=>'tools'
+        ],function () {
+            Route::resource('bigactivities', 'BigactivityController');
+        Route::group([
+                'middleware' => ['position'],
+                'position' => ['Root', 'Team Leader', 'Operation Manager']
+            ],function () {
+            Route::resource('applications', 'ApplicationController');
+            Route::resource('applicationtypes', 'ApplicationtypeController');
+            Route::resource('activities', 'ActivityController');
+            Route::resource('activitystatus', 'ActivitystatusController');
+            Route::resource('activitytypes', 'ActivitytypeController');
+            Route::resource('controlmpics', 'ControlmpicController');
+            Route::resource('departments', 'DepartmentController');
+            Route::resource('impacts', 'ImpactController');
+            Route::resource('organizations', 'OrganizationController');
+            Route::resource('personincharges', 'PersoninchargeController');
+            Route::resource('positions', 'PositionController');
+            Route::resource('remedygroups', 'RemedygroupController');
+            Route::resource('severities', 'SeverityController');
+            Route::resource('subdepartments', 'SubdepartmentController');
+    		Route::resource('teams', 'TeamController');
+            Route::resource('towerpics', 'TowerpicController');
+            Route::resource('users', 'UserController');
+        });
+    });
+// });
