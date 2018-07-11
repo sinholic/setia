5d5e43e0fe7709edd2cf3d8d0d1a565762468aa3
Mahar Prasetio <mahar@misua.id>
2018-07-12 06:58:08 +0700

    add yajra-dattables support

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,12 @@ Route::group([
         // Route::get('/', ['uses' => 'NewsController@index', 'as' => 'frontend.index']);
         Route::get('/detail/{id}', ['uses' => 'NewsController@detail', 'as' => 'frontend.detail']);
 });
+Route::group([
+        'prefix'=>'datatables'
+    ],function () {
+        // Route::get('/', ['uses' => 'NewsController@index', 'as' => 'frontend.index']);
+        Route::get('continent', ['uses' => 'DatatablesController@getContinentList', 'as' => 'datatables.continent']);
+});
 
 Route::group(['middleware' => ['auth']],function () {
     // Route::get('/', ['uses' => 'HomeController@index', 'as' => 'home.index']);
