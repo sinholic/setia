5f53b259551ad1d8034dc7cd701d60016800a47e
Mahar Prasetio <mahar@misua.id>
2018-07-28 17:46:16 +0700

    remove some widget

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,23 +1,6 @@
 @extends('admin.main')
 @section('content')
 <div class="col-md-6">
-    <div class="widget widget-nopad">
-        <div class="widget-header"> <i class="icon-bookmark fas fa-list-alt"></i>
-            <h3>Quick Post</h3>
-        </div>
-        <!-- /widget-header -->
-        <div class="widget-content">
-            <div class="widget big-stats-container">
-                <div class="widget-content">
-                    <div class="container">
-
-                    </div>
-                </div>
-                <!-- /widget-content -->
-
-            </div>
-        </div>
-    </div>
     <!-- /widget -->
     <div class="widget widget-nopad">
         <div class="widget-header"> <i class="icon-bookmark fas fa-list-alt"></i>
@@ -71,51 +54,32 @@
                     <i class="shortcut-icon fas fa-flag"></i>
                     <span class="shortcut-label">Negara</span>
                 </a>
-                <a href="javascript:;" class="shortcut">
+                <a href="{{ route('groupoperator.index') }}" class="shortcut">
                     <i class="shortcut-icon fas fa-signal"></i>
                     <span class="shortcut-label">Group Operator</span>
                 </a>
-                <a href="javascript:;" class="shortcut">
+                <a href="{{ route('operator.index') }}" class="shortcut">
                     <i class="shortcut-icon fas fa-mobile-alt "></i>
                     <span class="shortcut-label">Operator</span>
                 </a>
-                <a href="javascript:;" class="shortcut">
+                <a href="{{ route('telintarif.index') }}" class="shortcut">
                     <i class="shortcut-icon fas fa-money-bill "></i>
                     <span class="shortcut-label">Telin Tariff</span>
                 </a>
-                <a href="javascript:;" class="shortcut">
+                <a href="{{ route('kota.index') }}" class="shortcut">
                     <i class="shortcut-icon fas fa-building "></i>
                     <span class="shortcut-label">Kota</span>
                 </a>
-                <a href="javascript:;" class="shortcut">
+                <a href="{{ route('msc.index') }}" class="shortcut">
                     <i class="shortcut-icon fas fa-broadcast-tower"></i>
                     <span class="shortcut-label">MSC</span>
                 </a>
-                <a href="javascript:;" class="shortcut">
+                <a href="{{ route('exchangerate.index') }}" class="shortcut">
                     <i class="shortcut-icon fas fa-hand-holding-usd"></i>
                     <span class="shortcut-label">Exchange Rate</span>
                 </a>
             </div>
             <!-- /shortcuts -->
-        </div>
-        <!-- /widget-content -->
-    </div>
-    <!-- /widget -->
-    <div class="widget widget-table action-table">
-        <div class="widget-header"> <i class="icon-bookmark fas fa-th-list"></i>
-            <h3>Recent Continent</h3>
-        </div>
-        <!-- /widget-header -->
-        <div class="widget-content">
-            <table id="continent-table" class="table table-striped table-bordered">
-                <thead>
-                    <tr>
-                        <th> No </th>
-                        <th> Nama</th>
-                    </tr>
-                </thead>
-
-            </table>
         </div>
         <!-- /widget-content -->
     </div>
