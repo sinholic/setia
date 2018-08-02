52a7f20feb423962c1d8aaf49ed81ea1650ad46c
Soni <soni.setiabudi@gmail.com>
2018-08-02 15:03:43 +0700

    update page BI

--- app/Http/Controllers/DataBIController.php
+++ app/Http/Controllers/DataBIController.php
@@ -13,7 +13,7 @@ class DataBIController extends Controller
     {
         $menu_bi = Menu::leftjoin('a_group_menu', 'a_menu.id_group_menu', '=', 'a_group_menu.id')->where('a_menu.id','=',$id)->get();
         $categorynews = CategoryNews::get();
-        return view('frontend.handset', compact('categorynews','menu_bi'));
+        return view('frontend.dataBI', compact('categorynews','menu_bi'));
     }
 
 }
